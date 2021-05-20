8d320bf3c41772354619a266265acbdc02421ef2
unknown <shangeeran@gmail.com>
2021-05-20 23:43:45 +0530

    Add additional messages to hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
+
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
-
-  constructor(private messageService: MessageService) { }
 }
