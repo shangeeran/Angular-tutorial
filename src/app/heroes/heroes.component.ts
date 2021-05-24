7cce441da14091bb3975315058a7a0e57b41a9d3
unknown <shangeeran@gmail.com>
2021-05-24 23:07:35 +0530

    Navigating to hero details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,25 +9,39 @@ import { MessageService } from '../message.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+// export class HeroesComponent implements OnInit {
+
+//   selectedHero?: Hero;
+
+//   heroes: Hero[] = [];
+
+//   constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+//   ngOnInit() {
+//     this.getHeroes();
+//   }
+
+//   onSelect(hero: Hero): void {
+//     this.selectedHero = hero;
+//     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+//   }
+
+//   getHeroes(): void {
+//     this.heroService.getHeroes()
+//         .subscribe(heroes => this.heroes = heroes);
+//   }
+// }
 export class HeroesComponent implements OnInit {
-
-  selectedHero?: Hero;
-
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
